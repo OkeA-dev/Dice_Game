372fab6a49fdb1aea47bbd7d2de430878468beab
Oke.A <Okea.dev@gmail.com>
2024-06-28 17:31:06 +0100

    ...first commit...

--- README.md
+++ README.md
@@ -1,66 +1,11 @@
-## Foundry
+# Smart Contract-Based Dice Game
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
+## About
+This code is to create a game that leverage smart contracts to manage betting, payouts, and the outcome of the dice roll, ensuring transparency and security through blockchain technology.
 
-Foundry consists of:
+## What we want it to do?
 
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
-
-## Documentation
-
-https://book.getfoundry.sh/
-
-## Usage
-
-### Build
-
-```shell
-$ forge build
-```
-
-### Test
-
-```shell
-$ forge test
-```
-
-### Format
-
-```shell
-$ forge fmt
-```
-
-### Gas Snapshots
-
-```shell
-$ forge snapshot
-```
-
-### Anvil
-
-```shell
-$ anvil
-```
-
-### Deploy
-
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
-
-### Cast
-
-```shell
-$ cast <subcommand>
-```
-
-### Help
-
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
-```
+1. Allow user to set bet by staking any amount greater than 0.001 ethers.
+    1. If user predict right,he would won twice of his initial stake and withdraw successful.
+    2. Else he would loss his initial stake and won't have asset to withdraw function.
+2. Using Chainlink VRF -> Randomness.
---
+++ src/Dice.sol
@@ -0,0 +1,82 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title A simple Dice_game Contract
+ * @author Oke Abdulquadri
+ * @notice This contract is for creating a simple dice game
+ * @dev Implement Chainlink VRFv2
+ * 
+ */
+
+
+contract DiceGame {
+
+    error DiceGame__NotEnoughEthSent(uint256 leastAmount);
+    error DiceGame__NoStakeRecorded();
+    error DiceGame__TransferFailed();
+
+    enum StakeStatus {
+        YES,
+        NO
+    }
+
+    uint256 private immutable i_leastAmount;
+    StakeStatus private s_stakeStatus;
+    mapping (address => uint256) private s_players;
+
+    event CurrentPlayer(address indexed player, uint256 amoutStaked);
+
+    constructor(uint256 _leastAmount) {
+        i_leastAmount = _leastAmount;
+    }
+    function stakeBet() external payable {
+        if(msg.value < i_leastAmount) {
+            revert DiceGame__NotEnoughEthSent(i_leastAmount);
+        }
+        s_players[msg.sender] += msg.value;
+        s_stakeStatus = StakeStatus.YES;
+
+        emit CurrentPlayer(msg.sender, msg.value);
+    }
+    
+    function unstakeBet() external payable {
+        if (s_stakeStatus != StakeStatus.YES) {
+            revert DiceGame__NoStakeRecorded();
+        }
+        uint256 amount = s_players[msg.sender];
+        s_stakeStatus = StakeStatus.NO;
+        s_players[msg.sender] = 0;
+        (bool success,) = msg.sender.call{value: amount}("");
+        if (!success) {
+            revert DiceGame__TransferFailed();
+        }
+    }
+    function playDice() external {
+        if (s_stakeStatus != StakeStatus.YES) {
+            revert DiceGame__NoStakeRecorded();
+        }
+    }
+    function winnerWithdrawal() external {}
+    function OwnerWithdrawal() external {}
+}
